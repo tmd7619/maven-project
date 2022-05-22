91f5f68eca0865b2fb36025292ff3e649e586347
tmd7619 <tmd814@naver.com>
2022-05-22 16:21:22 +0900

    add JUnit Test

--- src/test/java/kr/ac/kopo/BoardTest.java
+++ src/test/java/kr/ac/kopo/BoardTest.java
@@ -1,7 +1,8 @@
 package kr.ac.kopo;
 
-import java.util.List;
-
+import kr.ac.kopo.board.dao.BoardDAO;
+import kr.ac.kopo.board.service.BoardService;
+import kr.ac.kopo.board.vo.BoardVO;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,40 +11,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import kr.ac.kopo.board.dao.BoardDAO;
-import kr.ac.kopo.board.service.BoardService;
-import kr.ac.kopo.board.vo.BoardVO;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:config/spring/spring-mvc.xml"})
 public class BoardTest {
-	
-	
-	@Autowired
-	private BoardDAO boardDAO;
-	//private SqlSessionTemplate sqlSessionTemplate;
-	
-	@Autowired
-	private BoardService boardService;
-	
-	@Autowired
-	private SqlSessionTemplate sessionTemplate;
-	
-	@Ignore
-	@Test
-	public void 전체게시글조회Test() throws Exception {
+
+
+    @Autowired
+    private BoardDAO boardDAO;
+    //private SqlSessionTemplate sqlSessionTemplate;
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private SqlSessionTemplate sessionTemplate;
+
+    //@Ignore
+    @Test
+    public void 전체게시글조회Test() throws Exception {
 //		List<BoardVO> list = boardDAO.searchAll(); // boardDAO가 sqlSessionTemplate 객체를 가지고 있기 때문에 이 방법으로 실행 가능
-		List<BoardVO> list = boardService.selectAllBoard();
-		for(BoardVO board : list) {
-			System.out.println(board);
-		}
-	}
-	
-	@Test
-	public void 상세게시글조회Test() throws Exception{
+        List<BoardVO> list = boardService.selectAllBoard();
+        for (BoardVO board : list) {
+            System.out.println(board);
+        }
+    }
+
+    @Ignore
+    @Test
+    public void 상세게시글조회Test() throws Exception {
+
+        BoardVO board = sessionTemplate.selectOne("board.BoardDAO.selectByNo", 121);
 		
-		BoardVO board = sessionTemplate.selectOne("board.BoardDAO.selectByNo", 121);
-		
-	}
+    }
 
 }
--- src/test/java/kr/ac/kopo/MemberTest.java
+++ src/test/java/kr/ac/kopo/MemberTest.java
@@ -1,9 +1,6 @@
 package kr.ac.kopo;
 
-import static org.junit.Assert.assertNotNull;
-
-import javax.sql.DataSource;
-
+import kr.ac.kopo.member.vo.MemberVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -11,29 +8,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import kr.ac.kopo.member.vo.MemberVO;
+import javax.sql.DataSource;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 실행할 때 마다 JUnit으로 실행하기 위해 설정
 @ContextConfiguration(locations = {"classpath:config/spring/spring-mvc.xml"}) // spring-mvc.xml 파일 읽어오기 
 public class MemberTest {
-	
-	
-	@Autowired
-	private DataSource dataSource ; // spring-mvc.xml에 만든 객체를 MybatisTest 클래스 안에 주입시킴
-	
-	@Autowired
-	private SqlSessionTemplate sessionTemplate;
-	
-	@Test
-	public void 로그인테스트()	throws Exception{
-		
-		MemberVO member = new MemberVO();
-		member.setId("admin");
-		member.setPassword("admin");
-		
-		MemberVO userVO = sessionTemplate.selectOne("member.MemberDAO.login", member);
-		assertNotNull(userVO);
-		
-	}
+
+
+    @Autowired
+    private DataSource dataSource; // spring-mvc.xml에 만든 객체를 MybatisTest 클래스 안에 주입시킴
+
+    @Autowired
+    private SqlSessionTemplate sessionTemplate;
+
+    @Test
+    public void 로그인테스트() throws Exception {
+
+        MemberVO member = new MemberVO();
+        member.setId("test");
+        member.setPassword("1234");
+
+        MemberVO userVO = sessionTemplate.selectOne("member.MemberDAO.login", member);
+        assertNotNull(userVO);
+
+    }
 
 }
